63f68ee2cd0a62a3eaeebf20f8f151378b8bd961
Kat Cosgrove <kat.cosgrove@gmail.com>
2017-11-08 18:42:26 -0800

    fdsfsd

--- scripts/views/book-view.js
+++ scripts/views/book-view.js
@@ -2,15 +2,44 @@
 
 var app = app || {};
 
-(function(module){
+(function(module) {
   const bookView = {};
-  bookView.initIndexPage = function() {
+
+  function reset() {
     $('.container').hide();
+    $('.navigation').slideDown(350);
+  }
+
+  bookView.initIndexPage = function() {
+    reset();
     $('.book-view').show();
-    module.Book.all.forEach(book => $('#book-list').append(book.toHtml()));
-  };
+    $('#book-list').show();
+    app.Book.all.map(book => $('#book-list-template').append(book.toHtml()));
+  }
+
+  // This function SHOULD be triggered through the page.js routing for /add, making the form visible and passing the form's data to a new Book object. That Book object is then passed to a createBook method on books.js.
+  bookView.initAddForm = function() {
+    reset();
+    $('.add-view').show();
+    $('#add-form').on('submit', function(event) {
+      event.preventDefault();
+
+      let book = {
+        title: event.target.title.value,
+        description: event.target.author.value,
+        category: event.target.isbn.value,
+        contact: event.target.image_url.value,
+        status: event.target.description.value,
+      };
+      console.log('book', book);
+
+      module.Book.createBook(book);
+    })
+  }
 
   module.bookView = bookView;
-})(app);
+})(app)
 
-$(app.Book.fetchAll(app.bookView.initIndexPage));
+$(function() {
+  app.Book.fetchAll(app.bookView.initIndexPage);
+})
