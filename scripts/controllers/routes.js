dcdda4c50b517488d87457cca42631a3f163e8e6
Kat Cosgrove <kat.cosgrove@gmail.com>
2017-11-08 17:02:45 -0800

    rebuild

---
+++ scripts/controllers/routes.js
@@ -0,0 +1,7 @@
+'use strict';
+
+page('/', ctx => app.Book.fetchAll(app.bookView.initIndexPage));
+page('/books/new', ctx => app.bookView.initAddForm(ctx));
+page('/books/:book_id', ctx => app.Book.fetchOne(ctx, app.detailView.initIndexPage));
+
+page();
---
+++ scripts/views/book-view.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var app = app || {};
+
+(function(module) {
+  const bookView = {};
+
+  function reset() {
+    $('.container').hide();
+    $('.navigation').slideDown(350);
+  }
+
+  bookView.initIndexPage = function() {
+    reset();
+    $('.book-view').show();
+    $('#book-list').empty();
+    app.Book.all.map(book => $('#book-list').append(book.toHtml()));
+  }
+
+  bookView.initAddForm = function() {
+    reset();
+    $('.add-view').show();
+    $('#add-form').on('submit', function(event) {
+      event.preventDefault();
+
+      let book = {
+        title: event.target.title.value,
+        description: event.target.author.value,
+        category: event.target.isbn.value,
+        contact: event.target.image_url.value,
+        status: event.target.description.value,
+      };
+      console.log('book', book);
+
+      module.Book.createBook(book);
+    })
+  }
+
+  module.bookView = bookView;
+})(app)
+
+$(function() {
+  app.Book.fetchAll(app.bookView.initIndexPage);
+})
