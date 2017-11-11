801014ff0e6390ea8ac9c3e7ecc392835836fcd3
Kat Cosgrove <kat.cosgrove@gmail.com>
2017-11-11 13:21:43 -0800

    admin controls work

--- scripts/views/admin-view.js
+++ scripts/views/admin-view.js
@@ -3,29 +3,32 @@ var app = app || {};
 var __API_URL__ = 'http://localhost:3000';
 
 (function(module) {
-
   const adminView = {};
 
-  adminView.initAdminPage = function () {
-    $('#admin-form').on('submit', function (event) {
+  //Initializes the admin view. Hides the password form and shows the update/delete links once the correct password is entered.
+  adminView.initAdminPage = function() {
+    $('.admin-view').show();
+    $('#admin-form').off('submit');
+    $('#admin-form').on('submit', function(event) {
       event.preventDefault();
-      let token = event.target.password.value;
-      $.get(`${__API_URL__}/admin`, {token})
-        .then(() => {
-          localStorage.token = true;
-          $('#admin-login').hide();
+      let token = event.target.passcode.value;
+      $.get(`${__API_URL__}/api/v1/admin`, {token})
+        .then(res => {
+          localStorage.setItem('token', res);
+          $('.admin-view').hide();
           $('#admin-controls').show();
         })
-        .catch (()=> page('/'));
+        .catch(() => page('/'));
     })
-
+    adminView.verify();
   }
-
-  adminView.verify = function () {
-    if(!localStorage.token) console.log('You have no token!')
-    else console.log('token');
-    next();
+  //Verifies the presense of a token.
+  adminView.verify = function() {
+    if(!localStorage.token) {
+      console.log('No token.')
+    } else {
+      console.log('Token found.')
+    }
   }
-
   module.adminView = adminView;
 })(app)
